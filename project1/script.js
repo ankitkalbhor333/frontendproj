a7714e46f7ba99d80ba9615ffdae53e7fa1953ca
ankitkalbhor333 <ankitkalbhor3@gmail.com>
2025-04-05 17:43:07 +0530

    i have done my project

--- project1/script.js
+++ project1/script.js
@@ -4,6 +4,7 @@ const scroll = new LocomotiveScroll({
 });
 
 
+
 var elemC = document.querySelector("#elem-container")
 var fixed = document.querySelector("#fixed-image")
 elemC.addEventListener("mouseenter", function () {
@@ -12,6 +13,9 @@ elemC.addEventListener("mouseenter", function () {
 elemC.addEventListener("mouseleave", function () {
     fixed.style.display = "none"
 })
+
+
+
 var elems=document.querySelectorAll(".elem")
 elems.forEach(function(e){
     e.addEventListener("mouseenter", function(){
@@ -19,3 +23,53 @@ elems.forEach(function(e){
         fixed.style.backgroundImage=`url(${image})`
     })
 })
+
+
+
+
+
+
+function loaderAnimation() {
+    var loader = document.querySelector("#loader")
+    setTimeout(function () {
+        loader.style.top = "-100%"
+    }, 4200)
+}
+
+
+loaderAnimation()
+      var swiper = new swiper(".mySwiper", {
+        slidesPerView: "auto",
+        centeredSlides: true,
+        spaceBetween: 50,
+      
+      });
+
+
+      function menuAnimation() {
+
+        var menu = document.querySelector("nav h3")
+        var full = document.querySelector("#full-scr")
+        var navimg = document.querySelector("nav img")
+        var flag = 0
+        menu.addEventListener("click", function () {
+            if (flag == 0) {
+                full.style.top = 0
+                navimg.style.opacity = 0
+                flag = 1
+            } else {
+                full.style.top = "-100%"
+                navimg.style.opacity = 1
+                flag = 0
+            }
+        })
+    }
+
+    function loaderAnimation() {
+        var loader = document.querySelector("#loader")
+        setTimeout(function () {
+            loader.style.top = "-100%"
+        }, 4200)
+    }
+    loaderAnimation() 
+    menuAnimation() 
